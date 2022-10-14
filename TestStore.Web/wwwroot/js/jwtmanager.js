4ed3ceb881d858ede1237aad53050904ad84d785
Aleksandar Milijanovic <acathebest00@gmail.com>
2022-10-14 06:36:07 +0200

    Token's utils added.

---
+++ TestStore.Web/wwwroot/js/jwtmanager.js
@@ -0,0 +1,13 @@
+﻿
+const token = {
+    content() {
+        let strToken = localStorage.getItem("access");
+        let payload = strToken.split(".")[1];
+        if (payload) {
+            let json = JSON.parse(atob(payload));
+            return json;
+        } else return null;
+    }
+}
+
+export default token;
