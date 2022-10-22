f9be0549be9cadd96e8acc26b356a647b3fd8105
Aleksandar Milijanovic <acathebest00@gmail.com>
2022-10-22 11:03:35 +0200

    Specification value controller tested.

---
+++ TestStore.Tests/SpecificationValueValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStore.Application.Dto;
+using TestStore.Implementation.DataAccess;
+using TestStore.Implementation.Validators;
+
+namespace TestStore.Tests
+{
+    [TestClass]
+    public class SpecificationValueValidatorTests
+    {
+        private TestStoreDbContext _context;
+        private SpecificationValueValidator _validator;
+        public SpecificationValueValidatorTests()
+        {
+            _context = new TestStoreDbContext();
+            this._validator = new SpecificationValueValidator(_context);
+        }
+
+        [TestMethod]
+        public void ValidateDto_ValidationSuccessfull()
+        {
+            var dto = new SpecificationValueDto
+            {
+                SpecificationId = 1,
+                Value = "Nesto novo"
+            };
+            var result = this._validator.Validate(dto);
+            // assert
+            Assert.IsTrue(result.IsValid);
+
+        }
+        [TestMethod]
+        public void ValidateDto_ValidationUnsuccessfull()
+        {
+            var dto = new SpecificationValueDto
+            {
+                Value = "Nesto novo"
+            };
+            var result = this._validator.Validate(dto);
+            // assert
+            Assert.IsFalse(result.IsValid);
+
+        }
+    }
+}
---
+++ TestStore.Tests/UpdateSpecificationValueValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStore.Application.Dto;
+using TestStore.Implementation.Validators;
+
+namespace TestStore.Tests
+{
+    [TestClass]
+    public class UpdateSpecificationValueValidatorTests
+    {
+        private UpdateSpecificationValueValidator _validator;
+        public UpdateSpecificationValueValidatorTests()
+        {
+            this._validator = new UpdateSpecificationValueValidator(new Implementation.DataAccess.TestStoreDbContext());
+        }
+
+        [TestMethod]
+        public void ValidateDto_ValidationSuccessfull()
+        {
+            var dto = new SpecificationValueDto
+            {
+                SpecificationId = 1,
+                Value = "Wireless",
+                NewValue = "Wireless2"
+            };
+            var result = this._validator.Validate(dto);
+            Assert.IsTrue(result.IsValid);
+        }
+        [TestMethod]
+        public void ValidateDto_ValidationUnsuccessfull()
+        {
+            var dto = new SpecificationValueDto
+            {
+                SpecificationId = 1,
+                Value = "Wireless"
+            };
+            var result = this._validator.Validate(dto);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
