694e799748a1c3c49ec3ec1bc5c54ffed16ef85d
Aleksandar Milijanovic <acathebest00@gmail.com>
2022-10-22 11:32:24 +0200

    Navigation links displayed dynamically.

--- TestStore.Web/Components/NavigationMenu.cs
+++ TestStore.Web/Components/NavigationMenu.cs
@@ -4,10 +4,24 @@ using TestStore.Implementation.DataAccess;
 
 namespace TestStore.Web.ViewComponents
 {
-    public class NavigationMenuViewComponent : ViewComponent
+    public class NavigationMenu : ViewComponent
     {
-        private static  readonly TestStoreDbContext _context = new TestStoreDbContext(); 
+        private  readonly TestStoreDbContext _context;
+        public NavigationMenu(TestStoreDbContext context)
+        {
+            this._context = context;
+        }
 
+        public IViewComponentResult Invoke()
+        {
+            var links = this._context.NavLinks.Select(x => new NavLinkDto
+            {
+                Name = x.Name,
+                Action = x.Action,
+                Controller = x.Controller
+            }).ToList();
+            return View(links);
+        }
         //public static   Microsoft.AspNetCore.Mvc.ViewComponents.ViewViewComponentResult GetNavLinks()
         //{
         //    var navLinks = _context.NavLinks.Select(x => new NavLinkDto
