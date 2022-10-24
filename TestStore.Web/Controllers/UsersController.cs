60a5c666a461f052df05653b4acb62ad5b558d33
Aleksandar Milijanovic <acathebest00@gmail.com>
2022-10-24 09:23:16 +0200

    Controller created with appropriate endpoints.

---
+++ TestStore.Web/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestStore.Application.Dto;
+using TestStore.Application.Usecases.Commands;
+using TestStore.Implementation.Usecases;
+
+namespace TestStore.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UsecaseHandler _handler;
+
+        public UsersController(UsecaseHandler handler)
+        {
+            _handler = handler;
+        }
+       
+        [HttpGet]
+       public IActionResult Edit(int id)
+        {
+            return View();
+        } 
+        [HttpPatch]
+        public IActionResult Update([FromForm] UserDto dto, [FromServices] IUpdateUserCommand command)
+        {
+            this._handler.HandleCommand(command, dto);
+            return NoContent();
+        }
+    }
+}
